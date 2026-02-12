dd160666658b00c68ea657a26eb3beaa64c876f5
Chuck Meyer <chuck.meyer@algolia.com>
2026-02-11 21:09:05 -0500

    refactor: move chat agent ID to algolia config utilities

--- tcg-search/src/components/ChatAgent.jsx
+++ tcg-search/src/components/ChatAgent.jsx
@@ -1,17 +1,12 @@
 import { Chat } from 'react-instantsearch';
 import 'instantsearch.css/components/chat.css';
+import { chatAgentId } from '../utilities/algolia';
 import ChatItemComponent from './ChatItemComponent';
 
 export default function ChatAgent() {
-  const agentId = process.env.REACT_APP_ALGOLIA_CHAT_AGENT_ID || '58bbdc94-6755-40ca-a7e0-5ece59adeb73';
-
-  if (!process.env.REACT_APP_ALGOLIA_CHAT_AGENT_ID) {
-    console.warn('REACT_APP_ALGOLIA_CHAT_AGENT_ID is not set, using default agent ID');
-  }
-
   return (
     <Chat
-      agentId={agentId}
+      agentId={chatAgentId}
       itemComponent={ChatItemComponent}
     />
   );
